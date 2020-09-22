3dc4ae718e00790bfcf80e42e1ef62d39412169d
Jhon Marreros Guzman <j_marreros@hotmail.com>
2020-09-21 19:21:12 -0500

    Agregado texto documentacion shortcode

--- admin/partials/illantas-woo-screen-shortcode.php
+++ admin/partials/illantas-woo-screen-shortcode.php
@@ -1,2 +1,72 @@
-<h1 class="wp-heading-inline"> Shortcodes incluídos para Illantas </h1>
+<h1 class="wp-heading-inline"> Shortcode incluído para Illantas </h1>
 <hr class="wp-header-end">
+
+
+<h2>Sin parámetros</h2>
+
+<hr>
+
+<p>
+Se puede usar el shortcode directamente,<strong>sin parámetros</strong>.
+</p>
+<p>
+Por ejemplo:
+</p>
+<code>
+[filtroIllantas]
+</code>
+
+<p>
+Para usarlo de esta forma se recomienda tener una página llamada: <i>llantas</i>
+</p>
+
+<hr>
+
+<h2>Con parámetros</h2>
+
+<p>
+Este shortcode acepta <strong>dos parámetros</strong>: <i>marca</i> y <i>fabricante</i>
+</p>
+
+<hr>
+
+<p>
+Por ejemplo para la marca:
+</p>
+
+<code>
+[filtroIllantas marca='xxx']
+</code>
+
+
+<p>
+Tener en cuenta:
+</p>
+
+<ul>
+    <li>- Debe existir una página llamada: <i>llantas-para-xxx</i>, en donde xxx será la marca</li>
+    <li>- El nombre xxx de la marca debe contener sólo caracteres en minúsculas</li>
+    <li>- Si xxx tiene un espacio será reemplazado por un guion: -</li>
+</ul>
+
+<hr>
+
+<p>
+Por ejemplo para el fabricante:
+</p>
+
+<code>
+[filtroIllantas fabricante='yyy']
+</code>
+
+<p>
+Tener en cuenta:
+</p>
+
+<ul>
+    <li>- Debe existir una página llamada: <i>llantas-yyy</i>, en donde yyy será el fabricante</li>
+    <li>- El nombre yyy del fabricante debe contener sólo caracteres en minúsculas</li>
+    <li>- Si yyy tiene un espacio será reemplazado por un guion: -</li>
+</ul>
+
+
